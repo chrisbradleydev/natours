fac2f3d76fb4eed331e613fd8d7329899c9b2174
Christopher Bradley <11767079+chrisbradleydev@users.noreply.github.com>
2021-10-02 21:53:07 -0400

    add mongoose

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 const app = require('./app');
 
+const uri = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 const port = process.env.PORT || 3000;
+
+mongoose.connect(uri).then(
+    () => {
+        console.log('db connection successful');
+    },
+    err => {
+        console.error(err);
+    },
+);
 
 app.listen(port, () => {
     // eslint-disable-next-line no-console
