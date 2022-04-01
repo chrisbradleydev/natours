48ad28aca80a7ae6ea6b04099b77291e257ead66
Christopher Bradley <11767079+chrisbradleydev@users.noreply.github.com>
2022-03-31 21:40:21 -0400

    add mongoose document middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
     {
@@ -48,6 +49,7 @@ const tourSchema = new mongoose.Schema(
             type: Number,
             default: 0,
         },
+        slug: String,
         startDates: [Date],
         summary: {
             type: String,
@@ -67,6 +69,17 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
     return this.duration / 7;
 });
+
+// document middleware, runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
+});
+
+// tourSchema.post('save', (doc, next) => {
+//     console.log(doc);
+//     next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
