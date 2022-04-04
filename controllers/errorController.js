4dc0b4c1f71cbf0fab8558e15c6562fcb2455b72
Christopher Bradley <11767079+chrisbradleydev@users.noreply.github.com>
2022-04-03 20:12:02 -0400

    islolate development and production errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,37 @@
+const sendErrorDev = (error, res) => {
+    res.status(error.statusCode).json({
+        status: error.status,
+        message: error.message,
+        isOperational: error.isOperational,
+        stack: error.stack,
+    });
+};
+
+const sendErrorProd = (error, res) => {
+    if (error.isOperational) {
+        // send operational error to client
+        res.status(error.statusCode).json({
+            status: error.status,
+            message: error.message,
+        });
+    } else {
+        // withhold unknown errors from client
+        console.error('💥💥💥', error);
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went wrong!',
+        });
+    }
+};
+
 module.exports = (error, req, res, next) => {
     // console.log(error.stack);
     error.statusCode = error.statusCode || 500;
     error.status = error.status || 'error';
-    res.status(error.statusCode).json({
-        status: error.status,
-        message: error.message,
-    });
+
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(error, res);
+    } else if (process.env.NODE_ENV === 'production') {
+        sendErrorProd(error, res);
+    }
 };
