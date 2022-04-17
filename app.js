6ec24fff794d684f284e5b851b3f51b9f6bfcfaa
Christopher Bradley <11767079+chrisbradleydev@users.noreply.github.com>
2022-04-16 21:01:20 -0400

    add helmet and limit maximum request body size

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -9,10 +10,15 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // global middleware
+// set security HTTP headers
+app.use(helmet());
+
+// development logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
+// limit requests
 const limiter = rateLimit({
     max: 100,
     windowMs: 60 * 60 * 1000,
@@ -20,12 +26,24 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// body parser, reading data from body into req.body
+app.use(
+    express.json({
+        limit: '10kb',
+    }),
+);
+
+// serve static files
 app.use(express.static(`${__dirname}/public`));
+
+// test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
+    // console.log(req.headers);
     next();
 });
+
+// routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
