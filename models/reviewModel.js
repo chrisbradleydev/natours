3b3cf266d75c81b72243e8e6487b233e50093e29
Christopher Bradley <11767079+chrisbradleydev@users.noreply.github.com>
2022-04-17 13:08:50 -0400

    add review model

---
+++ models/reviewModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+    {
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+        },
+        rating: {
+            type: Number,
+            min: 1,
+            max: 5,
+        },
+        review: {
+            type: String,
+            required: [true, 'review is required'],
+        },
+        tour: [
+            {
+                type: mongoose.Schema.ObjectId,
+                ref: 'Tour',
+                required: [true, 'review must belong to a tour'],
+            },
+        ],
+        user: [
+            {
+                type: mongoose.Schema.ObjectId,
+                ref: 'User',
+                required: [true, 'review must belong to a user'],
+            },
+        ],
+    },
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
+    },
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
