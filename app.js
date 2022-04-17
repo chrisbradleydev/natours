7b9bd983f7772b2d752fa49b34e828eb01e4a085
Christopher Bradley <11767079+chrisbradleydev@users.noreply.github.com>
2022-04-16 21:24:47 -0400

    add hpp

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -40,6 +41,13 @@ app.use(mongoSanitize());
 
 // data sanitization against XSS
 app.use(xss());
+
+// prevent parameter pollution
+app.use(
+    hpp({
+        whitelist: ['difficulty', 'duration', 'maxGroupSize', 'price', 'ratingsAverage', 'ratingsQuantity'],
+    }),
+);
 
 // serve static files
 app.use(express.static(`${__dirname}/public`));
