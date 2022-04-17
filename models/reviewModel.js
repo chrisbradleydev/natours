7076c90a56e1cbc6d8af5dabee161899ea29150f
Christopher Bradley <11767079+chrisbradleydev@users.noreply.github.com>
2022-04-17 14:03:03 -0400

    populate reviews with tour and user

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -36,6 +36,15 @@ const reviewSchema = new mongoose.Schema(
     },
 );
 
+// query middleware
+reviewSchema.pre(/^find/, function (next) {
+    this.populate([
+        { path: 'tour', select: 'name' },
+        { path: 'user', select: 'name photo' },
+    ]);
+    next();
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
