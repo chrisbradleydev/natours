232abbb1be2a8320d8fddbf71eed1ebe7fc46085
Christopher Bradley <11767079+chrisbradleydev@users.noreply.github.com>
2022-04-23 17:44:46 -0400

    update index page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,13 @@
-const getIndex = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsyc = require('../utils/catchAsync');
+
+const getIndex = catchAsyc(async (req, res, next) => {
+    const tours = await Tour.find();
     res.render('index', {
         title: 'All tours',
+        tours,
     });
-};
+});
 
 const getTour = (req, res) => {
     res.render('tour', {
